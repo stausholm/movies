de09ad52b80b7931f96d69bbf5e4812bcac2a753
Morten <muffix26@gmail.com>
2021-10-02 12:45:11 +0200

    import data into main

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,20 @@ const {OMDB_PLOT_TYPES} = require('./lib/config')
 const FETCH_INTERVAL_MS = 1000
 const {saveToFile} = require('./lib/saveToFile')
 const {formatVideoObj} = require('./lib/format')
+const data = require('./data.json')
+
+data[0].subtitles = 'testsetset'
+return log(data[0])
 
 fetchVideoData('tt0606111', 'short')
-  .then(data => {
-    //console.log(data)
+  .then(res => {
+    //console.log(res)
     const formattedVideoObj = formatVideoObj({
       "title": "Purple Giraffe",
       "subtitles": "[None]",
       "languages": "[English]",
       "imdbId": "tt0606111"
-    }, data)
+    }, res)
 
     saveToFile('./','db.json',JSON.stringify(formattedVideoObj))
   })
