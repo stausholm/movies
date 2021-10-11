278c24de6bf853731f7753deea11e595ea694740
Morten <muffix26@gmail.com>
2021-10-11 14:42:19 +0200

    experimenting with vue.config.js pwa and webpack

--- frontend/movies-frontend/vue.config.js
+++ frontend/movies-frontend/vue.config.js
@@ -1,1 +1,16 @@
-module.exports = { css: { sourceMap: process.env.NODE_ENV === 'development' } };
+module.exports = {
+  chainWebpack: (config) => {
+    // https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-plugin
+    // properties used in index.html
+    config.plugin('html').tap((args) => {
+      args[0].title = 'title test';
+      args[0].mortenTest = 'mortenTest test';
+      return args;
+    });
+  },
+  pwa: {
+    // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+    name: 'pwa name test',
+  },
+  css: { sourceMap: process.env.NODE_ENV === 'development' },
+};
