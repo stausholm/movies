c632556054578cfc929ef94855b33c3af53aa1e1
Morten <muffix26@gmail.com>
2021-10-11 11:30:30 +0200

    vue cli - generate vue 3 app with vuex, router, typescript, pwa, sass, and eslint/prettier

---
+++ frontend/movies-frontend/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ wow }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus aut quas beatae, neque odit
+      sit cumque tempore, ullam temporibus mollitia, aperiam aliquam. Aliquid voluptas possimus
+      quisquam optio, voluptate dignissimos fuga.
+    </p>
+  </div>
+  <p>asdasd</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  data() {
+    return {
+      wow: 'asdasd',
+    };
+  },
+});
+</script>
