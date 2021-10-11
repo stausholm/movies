26c3f478b12e3e6c82a53b7165c085c98605bfde
Morten <muffix26@gmail.com>
2021-10-11 14:42:36 +0200

    experimenting with typescript and teleport

--- frontend/movies-frontend/src/views/About.vue
+++ frontend/movies-frontend/src/views/About.vue
@@ -2,11 +2,16 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>{{ wow }}</p>
+    <button @click="changeWow('12')">change wow</button>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus aut quas beatae, neque odit
       sit cumque tempore, ullam temporibus mollitia, aperiam aliquam. Aliquid voluptas possimus
       quisquam optio, voluptate dignissimos fuga.
     </p>
+    <button @click="showStuff = !showStuff">toggle teleport</button>
+    <teleport to="#teleporttarget" v-if="showStuff">
+      <p>showStuff aaa {{ showStuff }}</p>
+    </teleport>
   </div>
   <p>asdasd</p>
 </template>
@@ -17,7 +22,15 @@ export default defineComponent({
   data() {
     return {
       wow: 'asdasd',
+      numOrString: 25 as number | string,
+      showStuff: true as boolean,
     };
+  },
+  methods: {
+    changeWow(val: string) {
+      this.wow = val;
+      return val;
+    },
   },
 });
 </script>
