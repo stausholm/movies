e3b46c018599d09c71dc35b8d656095bc32da934
Morten <muffix26@gmail.com>
2021-12-14 23:03:13 +0100

    adds styles to errorPage

--- frontend/movies-frontend/src/views/ErrorPage.vue
+++ frontend/movies-frontend/src/views/ErrorPage.vue
@@ -1,13 +1,51 @@
 <template>
-  <div class="">
-    <h1>ErrorPage</h1>
-  </div>
+  <layout>
+    <div class="container">
+      <div class="error-notice">
+        <h1>404</h1>
+        <h2>Woah.. that wasn't supposed to happen</h2>
+        <p>Looks like you requested a page that doesn't exists</p>
+        <router-link :to="{ name: 'Home' }" class="btn btn--primary" replace>Go home!</router-link>
+      </div>
+    </div>
+  </layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Layout from '@/layouts/Main.vue';
 
 export default defineComponent({
   name: 'ErrorPage',
+  components: {
+    Layout,
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.error-notice {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20vh;
+  text-align: center;
+
+  h1 {
+    //color: #cacedb;
+    opacity: 0.37;
+    font-size: 3.75rem;
+    margin-bottom: 1.5625rem;
+  }
+  h2 {
+    font-weight: 400;
+    font-size: 1.75rem;
+    margin-bottom: 0.625rem;
+    //color: #3d5170;
+  }
+  p {
+    //color: #818ea3;
+    opacity: 0.67;
+  }
+}
+</style>
