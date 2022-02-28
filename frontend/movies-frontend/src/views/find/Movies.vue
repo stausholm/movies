1e1c028a2702e6a539bdf693b79086358730c1eb
Morten <muffix26@gmail.com>
2022-02-28 18:27:19 +0100

    inits content on Movies page

--- frontend/movies-frontend/src/views/find/Movies.vue
+++ frontend/movies-frontend/src/views/find/Movies.vue
@@ -23,6 +23,23 @@
     </div>
 
     <!-- TODO: a horizontal scroller for each genre. The section header should lead to a list page -->
+    <div v-for="genre in genres" :key="genre.name">
+      <div class="container">
+        <section-header
+          :title="genre.name"
+          :to="genre.preview.length === genre.totalResults ? null : genre.to"
+          class="mb"
+        />
+      </div>
+      <horizontal-scroller>
+        <horizontal-scroller-item v-for="item in genre.preview" :key="item.imdbId">
+          <div class="card">{{ item.imdbTitle }}</div>
+        </horizontal-scroller-item>
+        <horizontal-scroller-item v-if="genre.preview.length < genre.totalResults">
+          <router-link :to="genre.to" class="card">see all</router-link>
+        </horizontal-scroller-item>
+      </horizontal-scroller>
+    </div>
   </layout>
 </template>
 
@@ -33,6 +50,9 @@ import HeroApp from '@/components/HeroApp.vue';
 import SectionHeader from '@/components/SectionHeader.vue';
 import HorizontalScroller from '@/components/HorizontalScroller.vue';
 import HorizontalScrollerItem from '@/components/HorizontalScrollerItem.vue';
+import { GENRES } from '@/constants/Genres';
+import { RouteLocationRaw } from 'vue-router';
+import Movie from '@/types/Movie';
 
 export default defineComponent({
   name: 'Movies',
@@ -42,6 +62,38 @@ export default defineComponent({
     SectionHeader,
     HorizontalScroller,
     HorizontalScrollerItem,
+  },
+  data() {
+    return {
+      genres: [] as {
+        name: string;
+        to: RouteLocationRaw;
+        preview: Movie[];
+        totalResults: number;
+      }[],
+    };
+  },
+  created() {
+    const genres = Object.keys(GENRES)
+      .map((x) => {
+        return {
+          name: GENRES[x], // TODO translation
+          to: {
+            name: 'GenreList',
+            params: {
+              genre: GENRES[x],
+            },
+            // TODO: queryparam for only showing movies on GenreList page
+          } as RouteLocationRaw,
+          // TODO: call getContentByGenre
+          // TODO: filter out the genres where no movies exist under
+          preview: [],
+          totalResults: 100,
+        };
+      })
+      .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase())); // sort alphabetically
+
+    this.genres = genres;
   },
 });
 </script>
